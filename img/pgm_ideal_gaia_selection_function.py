18a60fe756d832720f143456c6529d90bae26c14
agabrown <agabrown@xs4all.nl>
2020-02-13 21:07:16 +0100

    pgms version 1

--- img/pgm_ideal_gaia_selection_function.py
+++ img/pgm_ideal_gaia_selection_function.py
@@ -13,8 +13,8 @@ import daft
 pgm = daft.PGM([5,5], origin=[0, 0], node_unit=1.5, grid_unit=2.5)
 
 pgm.add_node(daft.Node("Gsource", "$G_\mathrm{source}$", 3, 4.5))
-pgm.add_node(daft.Node("afxpobs", "AF/XP", 3, 3.5, aspect=1.5))
-pgm.add_node(daft.Node("afxprvsobs", "AF/XP/RVS", 3, 2.0, aspect=1.75))
+pgm.add_node(daft.Node("afxpobs", "AF/XP\nobservations", 3, 3.5, aspect=2.25))
+pgm.add_node(daft.Node("afxprvsobs", "RVS\nobservations", 3, 2.0, aspect=2.25))
 pgm.add_node(daft.Node("GRVSsource", "$G_\mathrm{RVS,source}$", 1, 2.0, aspect=1.75))
 pgm.add_node(daft.Node("scanlaw", "Commanded\nscan law", 1, 4.5, fixed=True, aspect=1))
 pgm.add_node(daft.Node("Glim", "$G_\mathrm{lim}$", 1, 3.5, fixed=True, aspect=1, offset=(0,6)))
@@ -27,8 +27,8 @@ pgm.add_edge("scanlaw", "afxpobs")
 pgm.add_edge("Glim", "afxpobs")
 pgm.add_edge("GRVSlim", "afxprvsobs")
 
-pgm.add_plate([2.3, 3, 2, 1], label="Astrometry/\nphotometry sample", position="bottom right", shift=0, fontsize=8)
-pgm.add_plate([2.3, 1.5, 2, 1], label="RVS sample", position="bottom right", shift=0, fontsize=8)
+pgm.add_plate([2.0, 3, 2.5, 1], label="Astrometry/\nphotometry sample", position="bottom right", shift=0, fontsize=8)
+pgm.add_plate([2.0, 1.5, 2.5, 1], label="RVS sample", position="bottom right", shift=0, fontsize=8)
 
 pgm.render()
 pgm.figure.savefig("pgm_ideal_gsf.pdf")
